21706d741c7da33d125b25155a529a930f90da81
SunSmileAZY <1157451283@qq.com>
2020-04-08 22:11:22 +0800

    拒绝策略：reject nack；请求响应模式

--- rabbitmq-native/src/main/java/com/pine/producerbalance/backupexchange/BackupExConsumer.java
+++ rabbitmq-native/src/main/java/com/pine/producerbalance/backupexchange/BackupExConsumer.java
@@ -12,34 +12,27 @@ import java.util.concurrent.TimeoutException;
 public class BackupExConsumer {
 
 
-    public static void main(String[] argv)
-            throws IOException, TimeoutException {
+    public static void main(String[] argv) throws IOException, TimeoutException {
 
         // 打开连接和创建频道，与发送端一样
         Connection connection = CommonUtils.getConnection();
         final Channel channel = connection.createChannel();
-        channel.exchangeDeclare(BackupExProducer.BAK_EXCHANGE_NAME,
-                BuiltinExchangeType.FANOUT,
+        channel.exchangeDeclare(BackupExProducer.BAK_EXCHANGE_NAME, BuiltinExchangeType.FANOUT,
                 true, false, null);
         // 声明一个队列
         String queueName = "fetchother";
-        channel.queueDeclare(queueName,
-                false,false,
-                false,null);
+        channel.queueDeclare(queueName, false,false, false,null);
 
-        channel.queueBind(queueName,
-                BackupExProducer.BAK_EXCHANGE_NAME, "#");
+        channel.queueBind(queueName, BackupExProducer.BAK_EXCHANGE_NAME, "#");
 
         System.out.println(" [*] Waiting for messages......");
 
         // 创建队列消费者
         final Consumer consumerB = new DefaultConsumer(channel) {
             @Override
-            public void handleDelivery(String consumerTag,
-                                       Envelope envelope,
+            public void handleDelivery(String consumerTag, Envelope envelope,
                                        AMQP.BasicProperties properties,
-                                       byte[] body)
-                    throws IOException {
+                                       byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
                 //记录日志到文件：
                 System.out.println( "Received ["
